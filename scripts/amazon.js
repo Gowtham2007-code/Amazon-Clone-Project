da061b005376937af7a1357218187a0cc34eb752
Gowtham <gowthampannem2007@gmail.com>
2026-06-03 11:50:48 +0530

    use modules for better organization of our code.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,7 @@
+import {cart, addToCart, updateCartQuantity} from '../data/cart.js';
+import {products} from '../data/products.js';
+
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -53,35 +57,13 @@ products.forEach((product) => {
     </div> 
     `; 
 });
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart-button')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
-
-      let matchingItem;
-      cart.forEach((item) => {
-        if(productId === item.productId)
-          matchingItem = item;
-      });
-
-      if(matchingItem)
-        matchingItem.quantity += 1;
-      else
-      {
-          cart.push({
-          productId,
-          quantity : 1
-        });
-      }
-
-      let cartQuantity = 0;
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      addToCart(productId);
+      updateCartQuantity();
     });
   });
